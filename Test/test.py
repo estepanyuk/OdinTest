230fbf062ab994642336c5b59de6c82928ccf0a0
estepanyuk <107544725+estepanyuk@users.noreply.github.com>
2024-01-22 20:44:45 +0700

    add def minus in test.py

--- Test/test.py
+++ Test/test.py
@@ -2,7 +2,12 @@
 def plus(a: int, b: int) -> int:
     return a + b
 
+#2 комит
+def minus(a: int, b: int) -> int:
+    return a - b
+
 if __name__ == '__main__':
     print(plus(2, 2))
+    print(minus(5, 2))
 
 
